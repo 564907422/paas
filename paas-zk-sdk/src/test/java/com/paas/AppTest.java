cf542533cd48dea7b3429f773d1a1d2d917b3321
xiaozq <xiaozq@bbtree.com>
2018-08-14 15:00:35 +0800

    调整代理仓库

--- paas-zk-sdk/src/test/java/com/paas/AppTest.java
+++ paas-zk-sdk/src/test/java/com/paas/AppTest.java
@@ -36,40 +36,40 @@ public class AppTest
      */
     public void testApp()
     {
-        testFun();
+//        testFun();
         assertTrue( true );
     }
 
 
-    private static void testFun() {
-        try {
-            ZKClient zKClient =new ZKClient("114.215.202.56:32181",3000);
-            if(!zKClient.exists("/liwx/test/1"))
-                zKClient.createNode("/liwx/test/1", "{a:1,b:2}");
-            System.out.println("fun:"+zKClient.getNodeData("/liwx/test/1"));
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+//    private static void testFun() {
+//        try {
+//            ZKClient zKClient =new ZKClient("114.215.202.56:32181",3000);
+//            if(!zKClient.exists("/liwx/test/1"))
+//                zKClient.createNode("/liwx/test/1", "{a:1,b:2}");
+//            System.out.println("fun:"+zKClient.getNodeData("/liwx/test/1"));
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//    }
 
-    }
-
-    private static void testWatch() {
-        try {
-            ZKClient zKClient =new ZKClient("114.215.202.56:32181",3000);
-            zKClient.getNodeData("/liwx/test/1", new Watcher(){
-                @Override
-                public void process(WatchedEvent event) {
-                    // TODO Auto-generated method stub
-
-                }
-
-            });
-
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-    }
+//    private static void testWatch() {
+//        try {
+//            ZKClient zKClient =new ZKClient("114.215.202.56:32181",3000);
+//            zKClient.getNodeData("/liwx/test/1", new Watcher(){
+//                @Override
+//                public void process(WatchedEvent event) {
+//                    // TODO Auto-generated method stub
+//
+//                }
+//
+//            });
+//
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
